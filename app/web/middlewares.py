9332e4235566f2669a5685ad8b8209111a356040
hedgehog <hedgehog97ivan@gmail.com>
2023-03-07 14:24:15 +0300

    Добавлена обработка ошибок запросов middlewares
    Добавлен файл 'utils.py' для вспомогательных функций
    
    Добавлена функция 'error_json_response' для упрощения работы с сообщениями об ошибках

--- app/web/middlewares.py
+++ app/web/middlewares.py
@@ -1,7 +1,49 @@
-from app.web.aiohttp_extansion import Application
+import json
 
+from aiohttp.web_exceptions import HTTPUnprocessableEntity, HTTPException
+from aiohttp.web_middlewares import middleware
 from aiohttp_apispec.middlewares import validation_middleware
+
+from .aiohttp_extansion import Application, Request
+from .utils import error_json_response
+
+HTTP_ERROR_CODES = {
+    400: "bad_request",
+    401: "unauthorized",
+    403: "forbidden",
+    404: "not_found",
+    405: "not_implemented",
+    409: "conflict",
+    500: "internal_server_error",
+}
+
+
+@middleware
+async def error_handling_middleware(request: Request, handler):
+    try:
+        response = await handler(request)
+        return response
+    except HTTPUnprocessableEntity as e:
+        return error_json_response(
+            http_status=400,
+            status="bad_request",
+            message=e.reason,
+            data=json.loads(e.text),
+        )
+    # TODO: Переделать реакцию 409 ошибки
+    except HTTPException as e:
+        return error_json_response(
+            http_status=e.status,
+            status=HTTP_ERROR_CODES[e.status],
+            message=str(e),
+        )
+    except Exception as e:
+        request.app.logger.error("Exception", exc_info=e)
+        return error_json_response(
+            http_status=500, status="internal server error", message=str(e)
+        )
 
 
 def setup_middlewares(app: Application):
+    app.middlewares.append(error_handling_middleware)
     app.middlewares.append(validation_middleware)
---
+++ app/web/utils.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from aiohttp.web_response import json_response as aiohttp_json_response
+
+
+def error_json_response(
+    http_status: int,
+    status: str = "error",
+    message: Optional[str] = None,
+    data: Optional[dict] = None,
+):
+    if data is None:
+        data = {}
+    return aiohttp_json_response(
+        status=http_status,
+        data={
+            "status": status,
+            "message": str(message),
+            "data": data,
+        },
+    )
